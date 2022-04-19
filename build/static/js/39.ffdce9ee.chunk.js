5ac270ca3370b058a598fb6fd2562deef296d014
crypto0390 <crypto0390@gmail.com>
2022-04-18 20:45:24 -0400

    refactor

---
+++ build/static/js/39.ffdce9ee.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfairside-dashboard"]=this["webpackJsonpfairside-dashboard"]||[]).push([[39],{1069:function(s,a){}}]);
+//# sourceMappingURL=39.ffdce9ee.chunk.js.map
---
+++ build/static/js/40.6d1f3ddb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfairside-dashboard"]=this["webpackJsonpfairside-dashboard"]||[]).push([[40],{1104:function(s,a){}}]);
+//# sourceMappingURL=40.6d1f3ddb.chunk.js.map
